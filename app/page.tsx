7482be03cc41f6824d56eadc01ada1457f32874d
lmkarger <lmkarger1@gmail.com>
2025-04-01 20:30:37 -0500

    Started project

---
+++ app/page.tsx
@@ -0,0 +1,31 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    
+      
+    <div className="flex justify-center items-start w-[65vw] mx-auto">
+      <div className="flex justify-center flex-col w-[40vw] text-center h-full bg-white border-2 border-[#BBB]">
+        <div className="text-left m-5">
+          <h1 className="font-bold text-xl">Top research opportunities for you</h1>
+          <p>Based on your profile and preferences</p>
+        </div>
+        <div className="flex border-[#BBB] border-t-2 w-full h-full">
+          <p className="m-5">hiasdkajsd;laskdja;lkdjas;ldkja;ldksj</p>
+        </div>
+        <div className="flex border-[#BBB] border-t-2 w-full h-full">
+          <p className="m-5">hiasdkajsd;laskdja;lkdjas;ldkja;ldksj</p>
+        </div>
+        <div className="flex border-[#BBB] border-t-2 w-full h-full">
+          <p className="m-5">hiasdkajsd;laskdja;lkdjas;ldkja;ldksj</p>
+        </div>
+        <div className="flex border-[#BBB] border-t-2 w-full h-full">
+          <p className="m-5">hiasdkajsd;laskdja;lkdjas;ldkja;ldksj</p>
+        </div>
+      </div>
+      <div className="flex justify-center flex-col w-[60vw] text-center h-full bg-white border-2 border-l-0 border-[#BBB]">
+        <p>hi</p>
+      </div>
+    </div>
+  );
+}
